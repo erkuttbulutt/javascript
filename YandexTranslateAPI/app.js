9807b1ca632f95ce1ef248dd13662f25cf88a566
Erkut <erkutbulut_0998@hotmail.com>
2022-07-23 23:20:30 +0300

    YandexTranslateAPI

---
+++ YandexTranslateAPI/app.js
@@ -0,0 +1,32 @@
+eventListeners();
+
+function eventListeners() {
+  document
+    .getElementById("translate-form")
+    .addEventListener("submit", translateWord);
+  document.getElementById("language").onchange = function () {
+    //* Arayüz işlemleri
+    ui.changeUI();
+  };
+}
+
+const translate = new Translate(
+  document.getElementById("word").value,
+  document.getElementById("language").value
+);
+const ui = new UI()
+
+function translateWord(e) {
+  translate.changeParameters(
+    document.getElementById("word").value,
+    document.getElementById("language").value
+  );
+  translate.translateWord(function (err, response) {
+    if (errll) {
+      console.log(err);
+    } else {
+      ui.displayTranslate(response)
+    }
+  });
+  e.preventDefault();
+}
---
+++ YandexTranslateAPI/translate.js
@@ -0,0 +1,30 @@
+function Translate(word, language) {
+  this.apiKey = "";
+  this.word = word;
+  this.language = language;
+  this.xhr = new XMLHttpRequest();
+}
+
+Translate.prototype.translateWord = function (callback) {
+  const endpoint = `
+  https://translate.yandex.net/api/v1.5/tr.json/translate?key=${this.apiKey}&text=${this.word}&lang=${this.language}
+    `;
+  this.xhr.open("GET", endpoint);
+
+  this.xhr.onload = () => {
+    if (this.xhr.status === 200) {
+      const json = JSON.parse(this.xhr.responseText);
+      const text = json.text[0];
+      callback(null, text);
+    } else {
+      callback("Bir hata oluştu", null);
+    }
+  };
+
+  this.xhr.send();
+};
+
+Translate.prototype.changeParameters = function (newWord, newLang) {
+  this.word = newWord;
+  this.language = newLang;
+};
